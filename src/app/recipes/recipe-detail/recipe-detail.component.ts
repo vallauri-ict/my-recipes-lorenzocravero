47f5c96fee1c3fdfe60f303a18c8429ec8f9a66c
lorenzocravero <l.cravero.1295@vallauri.edu>
2022-01-13 11:02:08 +0100

    ritorno a json server

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RecipeModel } from 'src/app/models/recipe.models';
 import { RecipeService } from 'src/app/shared/recipe.service';
+import { ShoppingListService } from 'src/app/shared/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,8 +10,12 @@ import { RecipeService } from 'src/app/shared/recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  constructor(public recipeService: RecipeService) { }
+  constructor(public recipeService: RecipeService, public shoppingList: ShoppingListService) { }
 
   ngOnInit(): void {}
+
+  sendToShoppingList(): void{
+    
+  }
 
 }
